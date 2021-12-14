df285d3c80f57cc4a35353fd2a29bdc0b5ac88f1
manjugoutham <gouthamy219@gmail.com>
2021-12-14 13:38:28 +0530

    Added LineComp

--- LineCompUC1.java
+++ LineCompUC1.java
@@ -1,4 +1,5 @@
 package employeewage;
+
 import java.util.Scanner;
 
 public class LineCompUC1 {
@@ -22,10 +23,33 @@ public class LineCompUC1 {
 		System.out.println("The length is "+"("+x1+","+y1+"),"+"("+x2+","+y2+")=======>"+len);
 	}
 	
+	static void Equals() {
+		CalLength();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Third Co-ordinates:");
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
+
+		System.out.println("Enter Fourth Co-ordinates:");
+		int x4 = sc.nextInt();
+		int y4 = sc.nextInt();
+
+	    len1 = (int) Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+		System.out.println("The length of Second line is:" + len1);
+        L2 = len1;
+		
+		if (L1.equals(L2)) {
+			System.out.println("Lines are equal");
+		} else 
+			System.out.println("Lines are not equals");
+		
+	}
 	
 	public static void main(String args[])
 	{
-		CalLength();
+		Equals();
 	}
 
 }
+
